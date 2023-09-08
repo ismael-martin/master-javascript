e6d5b9d26bd234cf298f46ca2d64ebb6e09067cc
ismael-martin <azarquiel.99693@gmail.com>
2023-09-08 20:00:51 +0200

    08/09/2023

--- HOLA MUNDO/js/13-ejercicio3.js
+++ HOLA MUNDO/js/13-ejercicio3.js
@@ -12,6 +12,8 @@ while (!num2 || num2 == NaN){
 	num2 = Number(prompt("Dame el segundo número"));
 }
 
+
+
 if (num1 > num2){
     console.log("los numeros que hay entre "+num2+" y "+num1+" son");
     for(var i = num2+1; i < num1; i++){
@@ -29,4 +31,25 @@ if (num1 > num2){
 
 } else {
     console.log("Algo ha ido mal");
+}
+
+//otra forma de hacerlo con document.write mostrando los resultados directamente en pantalla en lugar de la consola
+
+if (num1 > num2){
+    document.write("<h1>De "+num2+" a "+num1+" están los siguientes numeros: </h1><br>");
+    for(var i = num2+1; i < num1; i++){
+        document.write(i+"<br>");
+    }
+
+} else if (num1 < num2){
+    document.write("<h1>De "+num2+" a "+num1+" están los siguientes numeros: </h1><br>");
+    for(var i = num1+1; i < num2; i++){
+        document.write(i+"<br>");
+    }
+
+} else if (num1 == num2){
+    document.write("<h1>"+num1+" y "+num2+" son iguales </h1>");
+
+} else {
+    document.write("Algo ha ido mal");
 }
